9ff60f8434fce36804951d937a1ab397bc4d9812
Stan Keathly <skeathly@gmail.com>
2020-04-14 09:11:42 -0500

    Update key logic

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyD2KWCnELrW5lS6nAFBohSgcXGKSxyJriY';
+const KEY = process.env.REACT_APP_YOUTUBE_KEY;
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
